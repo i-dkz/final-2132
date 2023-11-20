81cec4fcb411d65dab0bc0ebfad7dee657b2fabb
Zach Flentge-Wong <zachflentgewong@gmail.com>
2023-11-19 16:15:44 -0800

    feat: - added new image assets for transparency - jquery cdn - js classes created

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const alphabet = [
+    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
+]
+const bodyParts = [
+    "head", "torso", "right-leg", "left-leg", "right-arm", "left-arm"
+]
+
+class Game {
+
+}
