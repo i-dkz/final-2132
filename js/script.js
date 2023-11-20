729f913559211cb8dd1b7c01b533f9743105e047
Zach Flentge-Wong <zachflentgewong@gmail.com>
2023-11-19 21:54:05 -0800

    fix: - getting max wrong answers kills the man and takes user to tobey maguire

--- js/script.js
+++ js/script.js
@@ -44,39 +44,68 @@ const words = [
 ];
 
 class Game {
-    constructor() {
-      this.game = $(".game");
-      this.bodyPartsIndex = 0;
-      this.maxBodyPartsIndex = bodyParts.length - 1;
-  
-      this.game.append(`<img src="images/${bodyParts[this.bodyPartsIndex]}.png">`);
-    }
-  
-    updateBodyPart() {
-      if (this.bodyPartsIndex < this.maxBodyPartsIndex) {
-        this.bodyPartsIndex++;
-        $(".game img").attr("src", `images/${bodyParts[this.bodyPartsIndex]}.png`);
-      } else {
-        // Game over condition
+  constructor() {
+    this.game = $(".game");
+    this.bodyPartsIndex = 0;
+    this.maxBodyPartsIndex = bodyParts.length - 1;
+
+    this.game.append(
+      `<img src="images/${bodyParts[this.bodyPartsIndex]}.png">`
+    );
+  }
+
+  updateLastImage() {
+    this.bodyPartsIndex++;
+    $(".game img").attr("src", `images/${bodyParts[this.bodyPartsIndex]}.png`);
+  }
+
+  updateBodyPart() {
+    if (this.bodyPartsIndex < this.maxBodyPartsIndex - 1) {
+      this.bodyPartsIndex++;
+      console.log(this.bodyPartsIndex);
+      $(".game img").attr(
+        "src",
+        `images/${bodyParts[this.bodyPartsIndex]}.png`
+      );
+    } else {
+      // Update the last image
+      this.updateLastImage();
+      $(".key").removeClass(".key").addClass("key-pressed");
+      console.log(this.bodyPartsIndex, ": last one");
+
+      // Small delay before appending the spinning logo
+      setTimeout(() => {
         $("main").html(""); // Clear the inner HTML of the main container
-  
+
         // Add Batman spinning logo animation
         $("main").append(`
-        <img src="images/tobey.png" class="zoom-rotate">
-      `);
-  
-        // You can perform other actions here, such as resetting the game.
-      }
+          <img src="images/tobey.png" class="zoom-rotate">
+        `);
+
+        // Another delay before appending the "you let him die!" content
+        setTimeout(() => {
+          $("main").append(`
+            <h1>you let him die!</h1>
+            <span>HOW COULD YOU?</span>
+          `);
+          setTimeout(() => {
+            $("main").append(`
+                <div class="button">REDEEM YOURSELF</div>
+            `);
+          }, 1000);
+        }, 1500); // Adjust the delay duration as needed
+      }, 2000); // Set a minimal delay
     }
   }
-  
-  class Word {
-    constructor(game) {
-      this.word = words[1].split("");
-      this.game = game;
-  
-      this.word.forEach((element, index) => {
-        $(".cards").append(`
+}
+
+class Word {
+  constructor(game) {
+    this.word = words[1].split("");
+    this.game = game;
+
+    this.word.forEach((element, index) => {
+      $(".cards").append(`
           <div class="card" data-index="${index}">
             <div class="card-inner">
               <div class="card-face front">
@@ -88,67 +117,66 @@ class Game {
             </div>
           </div>
         `);
-      });
-    }
-  
-    flipCard(keyId) {
-      // Find all occurrences of the clicked key in the word
-      const matchingIndexes = this.word.reduce((indexes, letter, index) => {
-        if (letter === keyId) {
-          indexes.push(index);
-        }
-        return indexes;
-      }, []);
-  
-      if (matchingIndexes.length === 0) {
-        // No matching letters, update the body part
-        this.game.updateBodyPart();
-      }
-  
-      // Flip all corresponding cards
-      matchingIndexes.forEach((matchingIndex) => {
-        const card = $(`.card[data-index="${matchingIndex}"]`);
-        card.toggleClass("flipped");
-  
-        const isFlipped = card.hasClass("flipped");
-        const rotation = isFlipped ? "rotateY(180deg)" : "rotateY(0deg)";
-        card.find(".card-inner").css("transform", rotation);
-      });
-    }
-  }
-  
-  class Keyboard {
-    constructor(word) {
-      // Add click event listeners to all keys
-      $(".key").click(function (event) {
-        // Get the ID of the clicked key
-        const keyId = $(this).attr("id");
-  
-        // Check if the key has already been pressed
-        if (!$(this).hasClass("pressed")) {
-          // Call the flipCard method of the Word class based on the clicked key
-          word.flipCard(keyId);
-  
-          // Add a class to indicate that the key has been pressed
-          $(this).removeClass("key").addClass("key-pressed").addClass("pressed");
-  
-          // Remove the click event listener for this key
-          $(this).off("click");
-        }
-      });
-    }
-  }
-  
-  $(document).ready(function () {
-    const game = new Game();
-    const word = new Word(game);
-    const keyboard = new Keyboard(word);
-  });
-  
-
-  $(document).ready(function () {
-    $("main").on("animationend", ".zoom-rotate", function () {
-      // Animation has ended, you can perform additional actions if needed
-      console.log("Animation ended");
     });
+  }
+
+  flipCard(keyId) {
+    // Find all occurrences of the clicked key in the word
+    const matchingIndexes = this.word.reduce((indexes, letter, index) => {
+      if (letter === keyId) {
+        indexes.push(index);
+      }
+      return indexes;
+    }, []);
+
+    if (matchingIndexes.length === 0) {
+      // No matching letters, update the body part
+      this.game.updateBodyPart();
+    }
+
+    // Flip all corresponding cards
+    matchingIndexes.forEach((matchingIndex) => {
+      const card = $(`.card[data-index="${matchingIndex}"]`);
+      card.toggleClass("flipped");
+
+      const isFlipped = card.hasClass("flipped");
+      const rotation = isFlipped ? "rotateY(180deg)" : "rotateY(0deg)";
+      card.find(".card-inner").css("transform", rotation);
+    });
+  }
+}
+
+class Keyboard {
+  constructor(word) {
+    // Add click event listeners to all keys
+    $(".key").click(function (event) {
+      // Get the ID of the clicked key
+      const keyId = $(this).attr("id");
+
+      // Check if the key has already been pressed
+      if (!$(this).hasClass("pressed")) {
+        // Call the flipCard method of the Word class based on the clicked key
+        word.flipCard(keyId);
+
+        // Add a class to indicate that the key has been pressed
+        $(this).removeClass("key").addClass("key-pressed").addClass("pressed");
+
+        // Remove the click event listener for this key
+        $(this).off("click");
+      }
+    });
+  }
+}
+
+$(document).ready(function () {
+  const game = new Game();
+  const word = new Word(game);
+  const keyboard = new Keyboard(word);
+});
+
+$(document).ready(function () {
+  $("main").on("animationend", ".zoom-rotate", function () {
+    // Animation has ended, you can perform additional actions if needed
+    console.log("Animation ended");
   });
+});
