a181ff87b8c712e837a84409621ab83872b41170
Zach Flentge-Wong <zachflentgewong@gmail.com>
2023-11-25 17:04:08 -0800

    fix: submit button goes back to disabled after submission press

--- js/script.js
+++ js/script.js
@@ -1,31 +1,3 @@
-const alphabet = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-];
 const bodyParts = [
   "pole",
   "rope",
@@ -234,7 +206,7 @@ $(document).ready(function () {
     // Clear the input field
     letterInput.val("");
     // Remove the "submit" class after submission
-    submitButton.addClass("disabled");
+    submitButton.addClass("disabled").removeClass("submit");
   });
 });
 
